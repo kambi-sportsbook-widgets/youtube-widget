34890ed983b89d2c5d2fa6849fd47693b4251ab4
Rodrigo Carrasco <rodrigo@globalmouth.com>
2016-03-02 15:44:51 +0100

    KSBWI-191: Using new build tools

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,8 @@
 
    var dir = requireDir('./node_modules/widget-build-tools/');
 
+   var dir = requireDir('./node_modules/widget-aws-publish-tools/');
+
    // Add or override your gulp tasks here​
 
 }).call(this);
