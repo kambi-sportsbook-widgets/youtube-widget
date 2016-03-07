7007f8ecd07c373a65ad35625deecbd28a8f1a0b
Rodrigo Carrasco <rodrigo@globalmouth.com>
2016-03-07 11:20:21 +0100

    Removed AWS publish tools

--- gulpfile.js
+++ gulpfile.js
@@ -8,8 +8,6 @@
 
    var dir = requireDir('./node_modules/widget-build-tools/');
 
-   var dir = requireDir('./node_modules/widget-aws-publish-tools/');
-
    // Add or override your gulp tasks here​
 
 }).call(this);
