571aa62463159574b3c6da0e3f01487eee19e267
Rodrigo Carrasco <rodrigo@globalmouth.com>
2016-07-24 12:18:12 +0200

    KSBWI-459: Fixes for aspect ratio

--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,20 @@
 (function () {
    'use strict';
+
    var YoutubeWidget = CoreLibrary.Component.subclass({
       defaultArgs: {
          youtube: {
             origin: window.location.href,
-            height: 450,
+            height: '100%',
             width: '100%',
-            title: 'Barcelona Luis Suárez',
+            title: 'UEFA EURO 2016',
             playerVars: {
                hl: 'en',
                autoplay: 0,
                controls: 1,
                showinfo: 0,
                listType: 'playlist',
-               list: 'PLkksCTsYZQhGEDvHDfvb5BGZ3QVuQNF2C'
+               list: 'PLkksCTsYZQhFI3Niv9mtpEiRX6XZDy6El'
             }
          }
       },
@@ -24,14 +25,6 @@
 
       init () {
 
-         // Set a 16/9 ratio based on iframe width plus widget header height
-         var setRatio = debounce(function () {
-            // Holds main container width. Needed for ratio calculation
-            var mainContainerWidth = document.getElementById('main-container').offsetWidth;
-            let newHeight = 9 * (mainContainerWidth) / 16 + 37;
-            CoreLibrary.widgetModule.setWidgetHeight(newHeight);
-         }, 250);
-
          // Inject the Youtube SDK
          var loadYoutubeApi = () => {
             let tag = document.createElement('script');
@@ -39,11 +32,40 @@
             let firstScriptTag = document.getElementsByTagName('script')[0];
             firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
          };
-         // Creat a listener for window resize in order to resize the widget height
-         window.onresize = setRatio;
 
          // Load the youtube api framework
          loadYoutubeApi();
+
+         // Set a 16/9 ratio based on iframe width plus widget header height
+         var setRatio = debounce(function () {
+            // Holds main container width. Needed for ratio calculation
+            var mainContainerWidth = document.getElementById('main-container').offsetWidth;
+            let newHeight = ((9 / 16) * mainContainerWidth) + 37;
+            CoreLibrary.widgetModule.adaptWidgetHeight(newHeight);
+         }, 250);
+
+         // Create a listener for window resize in order to resize the widget height
+         window.onresize = setRatio;
+
+         // This runs when the Youtube API framework has loaded
+         window.onYouTubeIframeAPIReady = () => {
+            // Set initial width
+            setRatio();
+            // Get locale
+            let locale = CoreLibrary.config.locale.replace('_', '-');
+
+            if ( this.scope.args.youtube ) {
+               // Set locale
+               let playerVars = Object.assign(this.scope.args.youtube, { hl: locale });
+               let youtubeArgs = Object.assign(this.scope.args.youtube, playerVars);
+               this.scope.widgetTitle = this.scope.args.youtube.title;
+               // Load player
+               let player = new window.YT.Player('youtube_player', youtubeArgs);
+            } else {
+               // Remove widget if config fails
+               CoreLibrary.widgetModule.removeWidget();
+            }
+         };
 
          /**
           * Debounce function for resizing window
@@ -66,25 +88,6 @@
             };
          }
 
-         // This runs when the Youtube API framework has loaded
-         window.onYouTubeIframeAPIReady = () => {
-            // Set initial width
-            setRatio();
-            // Get locale
-            let locale = CoreLibrary.config.locale.replace('_', '-');
-
-            if ( this.scope.args.youtube ) {
-               // Set locale
-               let playerVars = Object.assign(this.scope.args.youtube, { hl: locale });
-               let youtubeArgs = Object.assign(this.scope.args.youtube, playerVars);
-               this.scope.widgetTitle = this.scope.args.youtube.title;
-               // Load player
-               let player = new window.YT.Player('youtube_player', youtubeArgs);
-            } else {
-               // Remove widget if config fails
-               CoreLibrary.widgetModule.removeWidget();
-            }
-         };
       }
 
    });
